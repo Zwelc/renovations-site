a2926ac25f5e1aa12c741102c7022092752b3fd8
Thurston <thurston@entersekt.com>
2023-04-05 21:54:04 +0200

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,35 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'renovations-site';
+
+  jobs = [
+    {
+      title: 'Building',
+      image: '/assets/pexels-brett-jordan-4692281.jpg',
+    },
+    {
+      title: 'Painting',
+      image: '/assets/pexels-stephanie-ho-994164.jpg',
+    },
+    {
+      title: 'Plastering',
+      image: '/assets/pexels-la-miko-3616762.jpg',
+    },
+    {
+      title: 'Tiling',
+      image: '/assets/pexels-henry-&-co-1144871.jpg',
+    },
+    {
+      title: 'Window Installations',
+      image: '/assets/pexels-ksenia-chernaya-5691526.jpg',
+    },
+    {
+      title: 'Door installations',
+      image: '/assets/pexels-henry-&-co-1144871.jpg',
+    },
+  ];
 }
